546306e0459172e8d861af6c155bd4fb60d4f8b9
rahmap <purwantiibuku@gmail.com>
2019-09-19 07:22:45 +0700

    Final

---
+++ custom.js
@@ -0,0 +1,22 @@
+
+function searchFn() {
+	var pencarian = document.getElementById('pencarian').value;
+	alert('Pencarian ' + pencarian + ' tidak ditemukan');
+	// console.log(document.getElementById('pencarian'));
+	// var val = document.getElementsById('j-search').innerHTML;
+} 
+
+function tips(value) {
+	var pencarian = document.getElementById('kett');
+	value = Rahma;
+
+	if (pencarian.style.display == "none") {
+		pencarian.style.display = "block";
+		pencarian.style.color = "red";
+	} else {
+		pencarian.style.display = "none";
+	}
+	
+	
+	// alert('Ini Adalah ' + value + x);
+}
